97e04cedbeeeb1f21f44e0ef7788297d86637129
Zylashro <dominik.jug@gmail.com>
2020-07-14 20:57:58 +0200

    Updated hideAndShow.js to hopefully function now take two.

--- static/src/hideAndShow.js
+++ static/src/hideAndShow.js
@@ -2,6 +2,8 @@
 const filterButton = document.getElementsByClassName("filter-by__btn");
 const filterContainer = document.getElementsByClassName("filter-by__container");
 
-filterButton.onclick = () => {
-    filterContainer.classList.replace("hidden", "");
-};
+filterButton.addEventListener("click", hideAndShow);
+
+function hideAndShow(event) {
+    filterContainer.classList.replace("hidden", "")
+}
