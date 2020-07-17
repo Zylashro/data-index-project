44e2eee52b471057f548ddfb9498fe97742f840b
Zylashro <dominik.jug@gmail.com>
2020-07-17 21:16:49 +0200

    Trying to fix a bad request error regarding the form take three.

--- app.py
+++ app.py
@@ -29,9 +29,9 @@ def enemy_index():
 
 @app.route('/enemy-list', methods=['POST'])
 def search_enemyIndex():
-    queryAll = request.form.get['show-all']
-    queryAttack = request.form.get['attack-type']
-    queryLevel = request.form.get['level-type']
+    queryAll = request.form.getlist['show-all']
+    queryAttack = request.form.getlist['attack-type']
+    queryLevel = request.form.getlist['level-type']
 
     enemyAttackMelee = mongo.db.enemyIndexMDB.find({'attack_type': 'melee'})
     enemyAttackRanged = mongo.db.enemyIndexMDB.find({'attack_type': 'ranged'})
