a44d22cb02a2083097a10070edbe83fe142aacb4
Zylashro <dominik.jug@gmail.com>
2020-08-05 18:53:09 +0200

    Updating debug to fix an internal server issue.

--- app.py
+++ app.py
@@ -313,4 +313,4 @@ def statistics():
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
-            debug=False)
+            debug=True)
