d2c38dfa3dc8b64ebe6777a2b682bbfe501487a8
Zylashro <dominik.jug@gmail.com>
2020-08-05 20:50:30 +0200

    Added a secret key in order for flash to work properly attempt two.

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app.config["MONGO_DBNAME"] = os.environ.get('MONGO_DBNAME')
 app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
 
 
-app.secret_key = os.environ.get('SECRET_KEY')
+app.secret_key = os.urandom(24)
 
 
 mongo = PyMongo(app)
